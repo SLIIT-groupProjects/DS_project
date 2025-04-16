b2dc000cc2d7f4dc6b3eb92c8129c2b18452a7da
Vihara Ching <liquinzeng@gmail.com>
2025-04-16 10:20:15 +0530

    merge conflicts resolve

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
@@ -11,14 +10,3 @@ export default {
   },
   plugins: [],
 };
-=======
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
->>>>>>> 88c4d419d1b13680505a3c029eff5406190298f9
