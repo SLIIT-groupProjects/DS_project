69d691bf8386404c7f3611a3715479864820ddc5
Vihara Ching <liquinzeng@gmail.com>
2025-04-16 11:54:33 +0530

    Revert "resolve"
    This reverts commit ef0a486e9b1f4f58c00b7ae2b23eadbf8c4dd300, reversing
    changes made to af59b02c3d72b7b8f61fce82370a123f23914a02.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'custom-bg': "url('https://res.cloudinary.com/fmart/image/upload/v1743648804/BGImage02_hzx5in.png')",
-      },
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
--- server/utils/jwtUtils.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const generateToken = (user) => {
-    return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
-};
