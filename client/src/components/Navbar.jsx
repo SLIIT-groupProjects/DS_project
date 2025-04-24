edfeef7f0b1cd0cb2e403b6657777f604920a16b
Vihara Ching <liquinzeng@gmail.com>
2025-04-24 16:59:57 +0530

    docker services implemented

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,17 +6,18 @@ import Swal from "sweetalert2";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(null); 
-  const location = useLocation(); 
-  const navigate = useNavigate(); 
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Check if user is logged in (using token) when the component mounts
     const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token); 
-  }, []); 
+    setIsLoggedIn(!!token);
+  }, []);
 
-  const isAuthPage = location.pathname === "/login" || location.pathname === "/register";
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/register";
 
   // Method to log out
   const handleLogout = () => {
@@ -26,29 +27,34 @@ const Navbar = () => {
       icon: "question",
       confirmButtonText: "Yes. Logout!",
       showCancelButton: true,
-      cancelButtonText: 'Cancel',
+      cancelButtonText: "Cancel",
       customClass: {
-        confirmButton: 'w-[400px] bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-lg transition duration-300',
-        cancelButton: 'w-[400px] bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-lg transition duration-300'
+        confirmButton:
+          "w-[400px] bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-lg transition duration-300",
+        cancelButton:
+          "w-[400px] bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-lg transition duration-300",
       },
-      }).then((result) => {
-        if (result.isConfirmed) {
-          localStorage.removeItem("token"); 
-          setIsLoggedIn(false); 
-          navigate("/login");
-        }
-      });
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
+        navigate("/login");
+      }
+    });
   };
 
   if (isLoggedIn === null) {
-    return null; 
+    return null;
   }
 
   return (
     <nav className="bg-orange-500 fixed top-0 left-0 right-0 z-50 text-white p-3 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <Link to="https://res.cloudinary.com/fmart/image/upload/v1743634267/FMLogo_nktw1c.png" className="flex items-center">
+        <Link
+          to="https://res.cloudinary.com/fmart/image/upload/v1743634267/FMLogo_nktw1c.png"
+          className="flex items-center"
+        >
           <img src={FMLogo} alt="Logo" className="w-12 h-12 mr-2" />
           <span className="text-2xl font-bold">FOOD MART</span>
         </Link>
@@ -57,21 +63,46 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-8">
           {isAuthPage ? (
             <>
-              <Link to="/register" className="hover:text-gray-200 font-bold">SIGN UP</Link>
-              <Link to="/login" className="hover:text-gray-200 font-bold">LOGIN</Link>
+              <Link to="/register" className="hover:text-gray-200 font-bold">
+                SIGN UP
+              </Link>
+              <Link to="/login" className="hover:text-gray-200 font-bold">
+                LOGIN
+              </Link>
+              <Link
+                to="/deliveryLogin"
+                className="hover:text-gray-200 font-bold"
+              >
+                DELIVERY
+              </Link>
             </>
           ) : isLoggedIn ? (
             <>
-              <Link to="/foods" className="hover:text-gray-200 font-bold">HOME</Link>
-              <Link to="/orders" className="hover:text-gray-200 font-bold">ORDERS</Link>
-              <Link to="/payments" className="hover:text-gray-200 font-bold">PAYMENTS</Link>
-              <button 
+              <Link to="/foods" className="hover:text-gray-200 font-bold">
+                HOME
+              </Link>
+              <Link to="/orders" className="hover:text-gray-200 font-bold">
+                ORDERS
+              </Link>
+              <Link to="/payments" className="hover:text-gray-200 font-bold">
+                PAYMENTS
+              </Link>
+              <Link
+                to="/deliveryLogin"
+                className="hover:text-gray-200 font-bold"
+              >
+                DELIVERY
+              </Link>
+              <button
                 onClick={handleLogout}
                 className="hover:text-gray-200 font-bold"
               >
                 LOGOUT
               </button>
-              <Link to="/cart" className="hover:text-gray-200 flex items-center font-bold">
+              <Link
+                to="/cart"
+                className="hover:text-gray-200 flex items-center font-bold"
+              >
                 <FiShoppingCart className="ml-5 text-2xl" />
               </Link>
             </>
@@ -91,17 +122,56 @@ const Navbar = () => {
         <div className="md:hidden bg-orange-500 text-white p-4 flex flex-col space-y-2">
           {isAuthPage ? (
             <>
-              <Link to="/register" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>SIGN UP</Link>
-              <Link to="/login" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>LOGIN</Link>
+              <Link
+                to="/register"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                SIGN UP
+              </Link>
+              <Link
+                to="/login"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                LOGIN
+              </Link>
             </>
           ) : isLoggedIn ? (
             <>
-              <Link to="/foods" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>HOME</Link>
-              <Link to="/orders" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>ORDERS</Link>
-              <Link to="/payments" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>PAYMENTS</Link>
-              <Link to="/cart" className="hover:text-gray-200 font-bold" onClick={() => setIsOpen(false)}>CART</Link>
-              <button 
-                onClick={() => { handleLogout(); setIsOpen(false); }}
+              <Link
+                to="/foods"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                HOME
+              </Link>
+              <Link
+                to="/orders"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                ORDERS
+              </Link>
+              <Link
+                to="/payments"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                PAYMENTS
+              </Link>
+              <Link
+                to="/cart"
+                className="hover:text-gray-200 font-bold"
+                onClick={() => setIsOpen(false)}
+              >
+                CART
+              </Link>
+              <button
+                onClick={() => {
+                  handleLogout();
+                  setIsOpen(false);
+                }}
                 className="hover:text-gray-200 font-bold flex items-right"
               >
                 LOGOUT
